2d3534a9dd168b8412bfc8119610f7f508aa1b0c
Jeremy Thai <github@jetha.anonaddy.com>
2022-11-20 16:25:26 -0500

    Removed setup.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,16 @@
-### RUN SETUP.SH FIRST (ONE TIME ONLY)
+## FETCH LATEST BUILD OF YCSB
+echo "Fetching YCSB latest build...\n\n"
+wget https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
+tar -xf ycsb-0.17.0.tar.gz
+
+echo "\n\n" 
+
+echo "SET UP OF CASSANDRA...\n\n"
+## CASSANDRA SETUP
+sh cassandra/cassandra_setup.sh
+
+echo "\n\n" 
+
 echo "TESTS ON CASSANDRA...\n\n"
 ## CASSANDRA BENCHMARK TESTS
 sh cassandra/cassandra_tests.sh
