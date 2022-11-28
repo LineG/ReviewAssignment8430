be8e151e465e8ff75cf5062a03634344092fb318
rayaneDZ <chlef-algerie@hotmail.fr>
2022-11-27 22:55:24 -0500

    added mongodb setup and tests scripts

---
+++ mongodb/mongodb_setup.sh
@@ -0,0 +1,24 @@
+sudo apt update
+
+#installing maven
+sudo apt install maven -y
+
+#installing docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+
+#commands to run docker without sudo
+sudo usermod -aG docker ${USER}
+su - ${USER}
+
+#installing docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+#run mongodb
+docker-compose -f docker-compose-mongodb.yml up
+sudo cat mongodb/configurehosts.txt >> /etc/hosts
