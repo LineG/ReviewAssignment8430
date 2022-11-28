f64b6df5ee178b2ea65977afe6b8c52030106801
rayaneDZ <chlef-algerie@hotmail.fr>
2022-11-28 00:20:26 -0500

    fixed mongodb tests

--- mongodb/mongodb_setup.sh
+++ mongodb/mongodb_setup.sh
@@ -1,7 +1,9 @@
+cd mongodb
 #adding python2 and setting it as python
-sudo apt-get install python2
+sudo apt-get install python2 -y
 sudo ln -s /bin/python2.7 /usr/bin/python
 
 #run mongodb
-docker-compose -f docker-compose-mongodb.yml up
-sudo cat mongodb/configurehosts.txt >> /etc/hosts
+sudo docker-compose -f docker-compose-mongodb.yml up -d
+cat configurehosts.txt | sudo tee -a /etc/hosts 
+cd ..
