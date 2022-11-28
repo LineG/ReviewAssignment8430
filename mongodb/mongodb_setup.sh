9f5f25b8011420b77e06af5b7d7e1d422ee4c076
rayaneDZ <chlef-algerie@hotmail.fr>
2022-11-27 22:57:13 -0500

    adding python2 to run docker-compose

--- mongodb/mongodb_setup.sh
+++ mongodb/mongodb_setup.sh
@@ -19,6 +19,10 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
+#adding python2 and setting it as python
+sudo apt-get install python2
+sudo ln -s /bin/python2.7 /usr/bin/python
+
 #run mongodb
 docker-compose -f docker-compose-mongodb.yml up
 sudo cat mongodb/configurehosts.txt >> /etc/hosts
