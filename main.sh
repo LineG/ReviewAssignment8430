fa572d93f74b9c99a3bf77c67faf9ab1127eab34
rayaneDZ <chlef-algerie@hotmail.fr>
2022-11-27 23:36:14 -0500

    minor change

--- main.sh
+++ main.sh
@@ -21,8 +21,6 @@ su - ${USER}
 ## installing docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-
 
 ## FETCH LATEST BUILD OF YCSB
 echo "Fetching YCSB latest build...\n\n"
