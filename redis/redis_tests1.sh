b4eaabe8c770a22f64b8a92f69b476149cbd4c68
Jeremy Thai <github@jetha.anonaddy.com>
2022-11-28 13:30:52 -0500

    Updated redis docker compose file to manage 6 nodes

--- redis/redis_tests1.sh
+++ redis/redis_tests1.sh
@@ -4,36 +4,36 @@ cd ycsb-0.17.0
 echo "\n\nLoading and running benchmark workload A...\n\n"
 
 echo "-------- WORKLOAD A -------- \n\n" 
-./bin/ycsb.sh load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_Load_a.txt
+./bin/ycsb.sh load redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_Load_a.txt
 
 echo "\n\n"
 
 echo "-------- WORKLOAD A -------- \n\n" 
-./bin/ycsb.sh run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_run_a.txt
+./bin/ycsb.sh run redis -s -P workloads/workloada -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_run_a.txt
 echo "\n\n" 
 
 # Workload B: Read mostly workload
 echo "\n\nLoading and running benchmark workload B...\n\n"
 
 echo "-------- WORKLOAD B -------- \n\n" 
-./bin/ycsb.sh load redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_Load_b.txt
+./bin/ycsb.sh load redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_Load_b.txt
 
 echo "\n\n" 
 
 echo "-------- WORKLOAD B -------- \n\n" 
-./bin/ycsb.sh run redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_run_b.txt
+./bin/ycsb.sh run redis -s -P workloads/workloadb -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_run_b.txt
 echo "\n\n"
 
 # Workload C: Read only
 echo "\n\nLoading and running benchmark workload C...\n\n"
 
 echo "-------- WORKLOAD C -------- \n\n"
-./bin/ycsb.sh load redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_Load_c.txt
+./bin/ycsb.sh load redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_Load_c.txt
 
 echo "\n\n" 
 
 echo "-------- WORKLOAD C -------- \n\n" 
-./bin/ycsb.sh run redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_run_c.txt
+./bin/ycsb.sh run redis -s -P workloads/workloadc -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_run_c.txt
 
 echo "\n\n" 
 
@@ -41,12 +41,12 @@ echo "\n\n"
 echo "\n\nLoading and running benchmark workload D...\n\n"
 
 echo "-------- WORKLOAD D -------- \n\n" 
-./bin/ycsb.sh load redis -s -P workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_Load_d.txt
+./bin/ycsb.sh load redis -s -P workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_Load_d.txt
 
 echo "\n\n"
 
 echo "-------- WORKLOAD D -------- \n\n" 
-./bin/ycsb.sh run redis -s -P workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_run_d.txt
+./bin/ycsb.sh run redis -s -P workloads/workloadd -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_run_d.txt
 echo "\n\n" 
 
 
@@ -54,11 +54,11 @@ echo "\n\n"
 echo "\n\nLoading and running benchmark workload E...\n\n"
 
 echo "-------- WORKLOAD E -------- \n\n" 
-./bin/ycsb.sh load redis -s -P workloads/workloade -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_Load_e.txt
+./bin/ycsb.sh load redis -s -P workloads/workloade -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_Load_e.txt
 echo "\n\n"
 
 echo "-------- WORKLOAD E -------- \n\n" 
-./bin/ycsb.sh run redis -s -P workloads/workloade -p "redis.host=127.0.0.1" -p "redis.port=6379" > ../outputs/tests_1/redis_run_e.txt
+./bin/ycsb.sh run redis -s -P workloads/workloade -p "redis.host=127.0.0.1" -p "redis.port=6379" -p "redis.cluster=true" > ../outputs/tests_1/redis_run_e.txt
 echo "\n\n" 
 
 cd ..
