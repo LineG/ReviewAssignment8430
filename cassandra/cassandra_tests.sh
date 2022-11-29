c05265277d789997f218b6e546c9b24d62924795
Jeremy Thai <github@jetha.anonaddy.com>
2022-11-29 13:39:32 -0500

    fixed incremental load for mongo and cassandra

--- cassandra/cassandra_tests.sh
+++ cassandra/cassandra_tests.sh
@@ -8,58 +8,58 @@ mkdir -p outputs/$output_folder
 # Workload A: Update heavy workload
 echo "\n\nLoading and running benchmark workload A...\n\n"
 
-echo "-------- WORKLOAD A -------- \n\n" >> outputs/$output_folder/cassandra_load_a.txt
-ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloada -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_load_a.txt
+echo "-------- WORKLOAD A -------- \n\n" > outputs/$output_folder/cassandra_load_a.txt
+ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloada -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_load_a.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_load_a.txt
 
-echo "-------- WORKLOAD A -------- \n\n" >> outputs/$output_folder/cassandra_run_a.txt
-ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloada -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_run_a.txt
+echo "-------- WORKLOAD A -------- \n\n" > outputs/$output_folder/cassandra_run_a.txt
+ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloada -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_run_a.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_run_a.txt
 
 
 # Workload B: Read mostly workload
 echo "\n\nLoading and running benchmark workload B...\n\n"
 
-echo "-------- WORKLOAD B -------- \n\n" >> outputs/$output_folder/cassandra_load_b.txt
-ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloadb -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_load_b.txt
+echo "-------- WORKLOAD B -------- \n\n" > outputs/$output_folder/cassandra_load_b.txt
+ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloadb -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_load_b.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_load_b.txt
 
-echo "-------- WORKLOAD B -------- \n\n" >> outputs/$output_folder/cassandra_run_b.txt
-ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloadb -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_run_b.txt
+echo "-------- WORKLOAD B -------- \n\n" > outputs/$output_folder/cassandra_run_b.txt
+ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloadb -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_run_b.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_run_b.txt
 
 
 # Workload C: Read only
 echo "\n\nLoading and running benchmark workload C...\n\n"
 
-echo "-------- WORKLOAD C -------- \n\n" >> outputs/$output_folder/cassandra_load_c.txt
-ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloadc -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_load_c.txt
+echo "-------- WORKLOAD C -------- \n\n" > outputs/$output_folder/cassandra_load_c.txt
+ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloadc -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_load_c.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_load_c.txt
 
-echo "-------- WORKLOAD C -------- \n\n" >> outputs/$output_folder/cassandra_run_c.txt
-ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloadc -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_run_c.txt
+echo "-------- WORKLOAD C -------- \n\n" > outputs/$output_folder/cassandra_run_c.txt
+ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloadc -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_run_c.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_run_c.txt
 
 
 # Workload D: Read latest workload
 echo "\n\nLoading and running benchmark workload D...\n\n"
 
-echo "-------- WORKLOAD D -------- \n\n" >> outputs/$output_folder/cassandra_load_d.txt
-ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloadd -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_load_d.txt
+echo "-------- WORKLOAD D -------- \n\n" > outputs/$output_folder/cassandra_load_d.txt
+ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloadd -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_load_d.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_load_d.txt
 
-echo "-------- WORKLOAD D -------- \n\n" >> outputs/$output_folder/cassandra_run_d.txt
-ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloadd -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_run_d.txt
+echo "-------- WORKLOAD D -------- \n\n" > outputs/$output_folder/cassandra_run_d.txt
+ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloadd -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_run_d.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_run_d.txt
 
 
 # Workload E: Short ranges
 echo "\n\nLoading and running benchmark workload E...\n\n"
 
-echo "-------- WORKLOAD E -------- \n\n" >> outputs/$output_folder/cassandra_load_e.txt
-ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloade -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_load_e.txt
+echo "-------- WORKLOAD E -------- \n\n" > outputs/$output_folder/cassandra_load_e.txt
+ycsb-0.17.0/bin/ycsb.sh load cassandra-cql -s -P ycsb-0.17.0/workloads/workloade -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_load_e.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_load_e.txt
 
-echo "-------- WORKLOAD E -------- \n\n" >> outputs/$output_folder/cassandra_run_e.txt
-ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloade -p hosts="127.0.0.1" > outputs/$output_folder/cassandra_run_e.txt
+echo "-------- WORKLOAD E -------- \n\n" > outputs/$output_folder/cassandra_run_e.txt
+ycsb-0.17.0/bin/ycsb.sh run cassandra-cql -s -P ycsb-0.17.0/workloads/workloade -p hosts="127.0.0.1" >> outputs/$output_folder/cassandra_run_e.txt
 echo "\n\n" >> outputs/$output_folder/cassandra_run_e.txt
--- mongodb/mongodb_tests.sh
+++ mongodb/mongodb_tests.sh
@@ -6,54 +6,54 @@ mkdir -p outputs/$output_folder
 # Workload A: Update heavy workload
 echo "\n\nLoading and running benchmark workload A...\n\n"
 
-echo "-------- WORKLOAD A LOAD-------- \n\n" >> outputs/$output_folder/mongodb_load_a.txt
+echo "-------- WORKLOAD A LOAD-------- \n\n" > outputs/$output_folder/mongodb_load_a.txt
 ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/workloada -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_load_a.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_load_a.txt
 
-echo "-------- WORKLOAD A RUN-------- \n\n" >> outputs/$output_folder/mongodb_run_a.txt
+echo "-------- WORKLOAD A RUN-------- \n\n" > outputs/$output_folder/mongodb_run_a.txt
 ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/workloada -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_run_a.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_run_a.txt
 
 # Workload B: Read mostly workload
 echo "\n\nLoading and running benchmark workload B...\n\n"
 
-echo "-------- WORKLOAD B LOAD-------- \n\n" >> outputs/$output_folder/mongodb_load_b.txt
+echo "-------- WORKLOAD B LOAD-------- \n\n" > outputs/$output_folder/mongodb_load_b.txt
 ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/workloadb -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_load_b.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_load_b.txt
 
-echo "-------- WORKLOAD B RUN-------- \n\n" >> outputs/$output_folder/mongodb_run_b.txt
+echo "-------- WORKLOAD B RUN-------- \n\n" > outputs/$output_folder/mongodb_run_b.txt
 ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/workloadb -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_run_b.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_run_b.txt
 
 # Workload C: Read only
 echo "\n\nLoading and running benchmark workload C...\n\n"
 
-echo "-------- WORKLOAD C LOAD-------- \n\n" >> outputs/$output_folder/mongodb_load_c.txt
+echo "-------- WORKLOAD C LOAD-------- \n\n" > outputs/$output_folder/mongodb_load_c.txt
 ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/workloadc -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_load_c.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_load_c.txt
 
-echo "-------- WORKLOAD C RUN-------- \n\n" >> outputs/$output_folder/mongodb_run_c.txt
+echo "-------- WORKLOAD C RUN-------- \n\n" > outputs/$output_folder/mongodb_run_c.txt
 ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/workloadc -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_run_c.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_run_c.txt
 
 # Workload D: Read latest workload
 echo "\n\nLoading and running benchmark workload D...\n\n"
 
-echo "-------- WORKLOAD C LOAD-------- \n\n" >> outputs/$output_folder/mongodb_load_d.txt
+echo "-------- WORKLOAD D LOAD-------- \n\n" > outputs/$output_folder/mongodb_load_d.txt
 ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/workloadd -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_load_d.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_load_d.txt
 
-echo "-------- WORKLOAD C RUN-------- \n\n" >> outputs/$output_folder/mongodb_run_d.txt
+echo "-------- WORKLOAD D RUN-------- \n\n" > outputs/$output_folder/mongodb_run_d.txt
 ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/workloadd -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_run_d.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_run_d.txt
 
 # Workload E: Short ranges
 echo "\n\nLoading and running benchmark workload E...\n\n"
 
-echo "-------- WORKLOAD C LOAD-------- \n\n" >> outputs/$output_folder/mongodb_load_e.txt
+echo "-------- WORKLOAD E LOAD-------- \n\n" > outputs/$output_folder/mongodb_load_e.txt
 ycsb-0.17.0/bin/ycsb.sh load mongodb -s -P ycsb-0.17.0/workloads/workloade -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_load_e.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_load_e.txt
 
-echo "-------- WORKLOAD C RUN-------- \n\n" >> outputs/$output_folder/mongodb_run_e.txt
+echo "-------- WORKLOAD E RUN-------- \n\n" > outputs/$output_folder/mongodb_run_e.txt
 ycsb-0.17.0/bin/ycsb.sh run mongodb -s -P ycsb-0.17.0/workloads/workloade -p recordcount=1000 -p mongodb.upsert=true -p mongodb.url=mongodb://mongo1:30001,mongo2:30002,mongo3:30003,mongo4:30004,mongo5:30005,mongo6:30006/?replicaSet=my-replica-set >> outputs/$output_folder/mongodb_run_e.txt
 echo "\n\n" >> outputs/$output_folder/mongodb_run_e.txt
